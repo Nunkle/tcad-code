5ff3136b869ea66893961cff04b7f731873849ea
Tim Poulsen <tim@skypanther.com>
2012-12-18 15:49:26 -0800

    db fix - autoincrement missing from create query

--- Finished_code/TiBH250/Resources/lib/db.js
+++ Finished_code/TiBH250/Resources/lib/db.js
@@ -1,6 +1,6 @@
 //bootstrap database
 var db = Ti.Database.open('TiBountyHunter');
-db.execute('CREATE TABLE IF NOT EXISTS fugitives(id INTEGER PRIMARY KEY, name TEXT, captured INTEGER);');
+db.execute('CREATE TABLE IF NOT EXISTS fugitives(id INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT, captured INTEGER);');
 db.close();
 
 exports.list = function(_captured) {
